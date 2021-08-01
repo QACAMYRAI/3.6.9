4aadfeb86e8b958f94ae1a51ee855a06095958a4
QACAMYRAI <86566403+QACAMYRAI@users.noreply.github.com>
2021-08-01 20:53:31 +0300

    BasketTest

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import pytest
+
+def test_basket(browser):
+    link = r'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.get(link)
+    basket = len(browser.find_elements_by_xpath(r'//button[@class="btn btn-lg btn-primary btn-add-to-basket"]'))
+    assert basket > 0
+    time.sleep(30)
